fa900b407efa21752826691084e8b56b6275d88a
IELTS-Arena <nafisdin31@gmail.com>
2026-04-29 01:26:46 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react'
 import LandingPage from './components/LandingPage'
 import ParaphrasingApp from './components/ParaphrasingApp'
+import TFNGApp from './components/TFNGApp'
 
 function App() {
-  const [started, setStarted] = useState(false)
+  const [view, setView] = useState('landing')
 
   return (
     <div>
-      {!started ? (
-        <LandingPage onStart={() => setStarted(true)} />
-      ) : (
-        <ParaphrasingApp />
+      {view === 'landing' && (
+        <LandingPage
+          onStartParaphrasing={() => setView('paraphrasing')}
+          onStartTFNG={() => setView('tfng')}
+        />
+      )}
+      {view === 'paraphrasing' && (
+        <ParaphrasingApp onBack={() => setView('landing')} />
+      )}
+      {view === 'tfng' && (
+        <TFNGApp onBack={() => setView('landing')} />
       )}
     </div>
   )
